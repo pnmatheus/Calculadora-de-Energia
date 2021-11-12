d775fc467aaf727fceced973801981f92e439804
Matheus Nascimento <contato.mpnascimento@gmail.com>
2021-11-11 22:14:44 -0300

    commit inicial

---
+++ Formulas.md
@@ -0,0 +1,43 @@
+### Fórmulas para o Cálculo da Conta de Energia 
+
+- **Uso do Sistema e Destribuição**
+
+  Qtd. kWh * Tarifa de Uso do Sistema e Destribuição (TUSD) c/Impostos
+
+  *Adotando TUSD c/Impostos = R$0,24332*
+
+- **Energia**
+
+  Qtd. kWh * Tarifa de Energia (TE) c/Impostos
+
+  *Adotando TE c/Impostos = R$ 0,3099*
+
+- **Adicional Bandeira Amarela**
+
+  Qtd. kWh * Tarifa da Bandeira Amarela
+
+  *Adotando Tarifa da Bandeira Amarela = R$0,00278947*
+
+  
+
+  Teremos então o ***Total Parcial*** igual a (**Uso do Sistema e Destribuição** + **Energia** + **Adicional Bandeira Amarela**)
+
+  
+
+- **PIS/Pasep**
+
+  Total Parcial 1 * %PIS/PASEP resultando em um Total Parcial 2
+
+  *Adotando PIS/PASEP = 0,92%*
+
+- **COFINS**
+
+  Total Parcial 2 * %COFINS resultando em um Total Parcial 3
+
+  *Adotando COFINS = 3,91%*
+
+- **COSIP**
+
+  Variavel de acordo com cada região, vamos adotar a de Barueri que é de R$ 4,40
+
+  Com isso temos o total do conta de engergial igual a (**Total Parcial 3** + **COSIP**)
---
+++ calculoEnergia.py
@@ -0,0 +1,64 @@
+'''
+Turma: 4ECR
+Alunos: 
+Felipe Torigoe Carrera 			    RM 79896
+Guilherme Fujii Ramalho Filgueiras 	RM 79932
+Matheus Issao Bocci 			    RM 81542
+Matheus Pereira Nascimento		    RM 81618
+Richard Rodrigues				    RM 80906
+'''
+
+tusd = 0.24332
+te = 0.3099
+bandeiraAmarela = 0.001874
+bandeiraVermelhaP1 = 0.003971
+bandeiraVermelhaP2 = 0.009492
+bandeiraEscassez = 0.1420
+pisPasep = 0.92
+cofins = 3.91
+cosip = 4.40
+icms = 12
+
+kwh = float(input('Insira o seu consumo no mês em kWh: '))
+
+print(f'Consumo do mês (kWh): {kwh}')
+
+opt = int(input('\nSelecione uma bandeira tarifária:'
+'\n1 - Bandeira Verde: sem adição de tarifa'
+f'\n2 - Bandeira Amarela: R${bandeiraAmarela} por kWh'
+f'\n3 - Bandeira Vermelha Patamar 1: R${bandeiraVermelhaP1} por kWh'
+f'\n4 - Bandeira Vermelha Patamar 2: R${bandeiraVermelhaP2} por kWh'
+f'\n5 - Bandeira Escassez Hídrica: R${bandeiraEscassez} por kWh'
+'\n\nDigite sua opção: '))
+
+if opt == 1:
+    bandeira = 0
+elif opt == 2:
+    bandeira = bandeiraAmarela
+elif opt == 3:
+    bandeira = bandeiraVermelhaP1
+elif opt == 4:
+    bandeira = bandeiraVermelhaP2
+elif opt == 5:
+    bandeira = bandeiraEscassez
+else:
+    print('\nBandeira inválida!')
+    exit()
+
+print(f'\n\nDetalhamento do Sistema Tarifario:'
+f'\nTarifa de Uso do Sistema e Destribuição (TUSD): R${tusd}* por kWh' 
+f'\nTarifa de Energia (TE): R${te}* por kWh\n'
+f'\nAdicional Bandeira Amarela: R${bandeiraAmarela} por kWh'
+f'\nPIS/PASEP: {pisPasep}%'
+f'\nCOFINS: {cofins}%'
+f'\nCOSIP Barueri: R${cosip}'
+f'\nICMS: {icms}%'
+'\n\n*Valores com impostos\n')
+
+parcial1 = ((tusd*kwh) + (te*kwh) + (bandeira*kwh)) * (1+(pisPasep/100))
+
+parcial2 = parcial1 * (1+(cofins/100))
+
+valorFinal = parcial2 + cosip
+
+print(f'\nO valor da conta de energia será de: R${valorFinal}\n')
