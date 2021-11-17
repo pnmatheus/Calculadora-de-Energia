c6551f22398d86a18b90b93668833eca1786303b
Matheus Nascimento <contato.mpnascimento@gmail.com>
2021-11-16 21:54:11 -0300

    Ajuste de formula

--- calculoEnergia.py
+++ calculoEnergia.py
@@ -14,8 +14,8 @@ bandeiraAmarela = 0.001874
 bandeiraVermelhaP1 = 0.003971
 bandeiraVermelhaP2 = 0.009492
 bandeiraEscassez = 0.1420
-pisPasep = 0.92
-cofins = 3.91
+pisPasep = 1.0092
+cofins = 1.0391
 cosip = 4.40
 icms = 12
 
@@ -49,8 +49,8 @@ print(f'\n\nDetalhamento do Sistema Tarifario:'
 f'\nTarifa de Uso do Sistema e Destribuição (TUSD): R${tusd}* por kWh' 
 f'\nTarifa de Energia (TE): R${te}* por kWh\n'
 f'\nAdicional Bandeira Amarela: R${bandeiraAmarela} por kWh'
-f'\nPIS/PASEP: {pisPasep}%'
-f'\nCOFINS: {cofins}%'
+f'\nPIS/PASEP: {round((pisPasep - 1)* 100, 2)}%'
+f'\nCOFINS: {round((cofins - 1)* 100, 2)}%'
 f'\nCOSIP Barueri: R${cosip}'
 f'\nICMS: {icms}%'
 '\n\n*Valores com impostos\n')
@@ -61,9 +61,11 @@ tp2 = te*kwh
 
 tp3 = bandeira*kwh
 
-tp4 = (tp1 + tp2 + tp3) * (1+(pisPasep/100))
+tp4 = tp1 + tp2 + tp3
 
-tp5 = tp4 * (1+(cofins/100))
+tp5 = tp4 * pisPasep
+
+tp6 = tp5 * cofins
 
 total = tp5 + cosip
 
